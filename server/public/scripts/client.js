3e504a96f51d756b332ade010e64b7f303deac00
Jeff Richardson <jjrich13@gmail.com>
2018-06-27 16:39:35 -0500

    Initial Goals Met

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,6 +12,22 @@ function addQuotesToDom(){
         method: 'GET'
     }).done(function(res){
         console.log(res);
-        
+        for (let i = 0; i < res.length; i++) {
+            $('#quoteHolder').append(`<div class="card">
+            <div class="card-header">
+              Quote
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>${res[i].text}</p>
+                <footer class="blockquote-footer">${res[i].author} <cite title="Source Title">${res[i].source}</cite></footer>
+              </blockquote>
+            </div>
+          </div>`)
+            
+        }
+    }).fail(function(errorRes){
+        console.log(errorRes);
+        alert('Request Failed, error ' + errorRes.status)
     })
 }
--- server/quotes.js
+++ server/quotes.js
@@ -1,12 +1,16 @@
 const quotes = [{
     text: 'So yes. It had flaws, but what does that matter when it comes to matters of the heart? We love what we love. Reason does not enter into it. In many ways, unwise love is the truest love. Anyone can love a thing because. That’s as easy as putting a penny in your pocket. But to love something despite. To know the flaws and love them too. That is rare and pure and perfect.',
-    author: 'Patrick Rothfuss'
+    author: 'Kvothe',
+    source: 'Patrick Rothfuss, Wise Mans Fear'
 },{
     text: 'Nobody exists on purpose, nobody belongs anywhere, everybody is going to die, come watch TV.',
-    author: 'Morty, from Rick & Morty'
+    author: 'Morty',
+    source: 'Rick & Morty'
+
 }, {
     text: 'Die! Die! Die!',
-    author: 'Reaper'
+    author: 'Reaper',
+    source: 'Overwatch'
 }
 ]
 
