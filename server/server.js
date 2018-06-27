63c9fa22bf29728da1e3a66298a6ae56d309822d
Jeff Richardson <jjrich13@gmail.com>
2018-06-27 16:13:03 -0500

    Quotes is now displaying on the dom at /quotes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 
 //require express
 const express = require('express');
+const quotes = require('./quotes.js');
 
 const app = express();
 const port = 5000
@@ -9,8 +10,14 @@ const port = 5000
 
 app.use(express.static('server/public'));
 
+app.get('/quotes', function(req, res){
+    res.send(quotes);
+})
+
 //start up the server
 app.listen(port, function(){
     console.log('listening on port', port);
     
-})
+});
+
+
